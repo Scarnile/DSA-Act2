bda383c3683781c0bfa623e46e898153a302a8b8
Scarnile <198330506+Scarnile@users.noreply.github.com>
2026-05-17 23:08:03 +0800

    first commit

---
+++ Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+using namespace std;
+
+int main() {        
+    string word;
+    std::cout << "Enter Input: "; 
+    std::cin >> word;
+
+    cout << word;
+    return 0;       
+}
