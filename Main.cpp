a7234c06e57c08793e5da5b14a14f314a8b44e64
Scarnile <198330506+Scarnile@users.noreply.github.com>
2026-05-17 23:17:08 +0800

    Add LetterOccurences struct

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,18 @@
 #include <iostream> 
 using namespace std;
 
+struct LetterOccurences{
+    string letter;
+    int occurences;
+};
+
 int main() {        
     string word;
+    LetterOccurences letters[2] = {
+        {"a", 3}, 
+        {"c", 4}  
+    };
+
     cout << "Enter Input: "; 
     cin >> word;
 
